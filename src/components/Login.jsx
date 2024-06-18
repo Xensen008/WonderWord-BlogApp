67f7df616a8d2b3a883ac3c8d78c4f01c693ebb4
Aryan Bhati <123343302+AryanBhati7@users.noreply.github.com>
2024-06-18 17:10:22 +0000

    feat: disabled btn when isSubmitting

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,7 +9,7 @@ import { useForm } from 'react-hook-form'
 function Login() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm()
     const [error, setError] = useState("")
 
     const login = async (data) => {
@@ -71,10 +71,11 @@ function Login() {
 
                     <div>
                         <Button
+                              disabled={isSubmitting}
                             type="submit"
                             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
-                            Sign in
+                           {isSubmitting ? "Signing in..." : "Sign in"} 
                         </Button>
                     </div>
                 </form>
