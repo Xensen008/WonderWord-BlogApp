8332654c83c4ea3665ee2d6b75ad79e72444af79
Xensen008 <arnabjyotikakati008@gmail.com>
2024-06-21 00:31:51 +0530

    syntax fixed

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -19,8 +19,11 @@ export default function Post() {
     useEffect(() => {
         if (slug) {
             appwriteService.getPost(slug).then((post) => {
-                if (post) setPost(post);
-                else navigate("/");
+                if (post){
+                    setPost(post);
+                }else {
+                 navigate("/");
+                }
             }).catch(() => navigate("/"));
         } else navigate("/");
     }, [slug, navigate]);
